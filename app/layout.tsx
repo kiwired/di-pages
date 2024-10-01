1e3f924d5584c8843da65507fcf9891f9ae1d782
Max D <maksimdoronin93@gmail.com>
2024-10-01 13:47:23 +0200

    feat: add yandex metrik

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next'
+import Script from 'next/script'
 import { Montserrat } from 'next/font/google'
 import './globals.css'
 
@@ -30,7 +31,32 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ru">
-      <body className={monsterat.className}>{children}</body>
+      <body className={monsterat.className}>
+
+        <Script id="yandex-metrika" strategy="beforeInteractive">
+          {`(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+            m[i].l=1*new Date();
+            for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+            k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+            (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+            ym(98511387, "init", {
+              clickmap: true,
+              trackLinks: true,
+              accurateTrackBounce: true,
+              webvisor: true
+            });
+          `}
+        </Script>
+
+        <noscript>
+          <div>
+            <img src="https://mc.yandex.ru/watch/98511387" style={{ position: 'absolute', left: '-9999px' }} alt="" />
+          </div>
+        </noscript>
+
+        {children}
+      </body>
     </html>
   )
 }
