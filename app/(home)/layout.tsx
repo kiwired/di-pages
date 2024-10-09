69363c83f406f6b81ed75ab40ad8a9eed4cd6411
Max D <maksimdoronin93@gmail.com>
2024-10-09 21:52:03 +0200

    feat: update home page

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -1,24 +1,19 @@
-import Image from 'next/image'
-import { ScrollReveal } from './scroll-reveal'
+import localFont from 'next/font/local'
+
+const evolventa = localFont({
+	src: [
+		{
+			path: './fonts/Evolventa-Regular.woff',
+			weight: '400',
+			style: 'normal',
+		},
+	]
+})
 
 export default function Layout({ children }: { children: React.ReactNode }) {
 	return (
-		<>
-
-			<ScrollReveal delay={1000}>
-				<div className="absolute top-0 left-0 w-full">
-					<Image
-						src='/logo.svg'
-						alt='Диана Позднякова - психолог, гештальт—терапевт'
-						width={215}
-						height={55}
-						className='w-max-full w-auto h-15 mx-auto my-5'
-					/>
-				</div>
-			</ScrollReveal>
-
-
+		<div className={evolventa.className}>
 			{children}
-		</>
+		</div>
 	)
 }
