947a3d4e29f399e8e7b9099cefac3eb279743fb8
Maxim D <maksimdoronin93@gmail.com>
2025-05-29 15:07:49 +0200

    feat: update text

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,12 +4,22 @@ const config: Config = {
 	content: ['./app/**/*.{js,ts,jsx,tsx,mdx}'],
 	theme: {
 		extend: {
+			colors: {
+				pink: {
+					DEFAULT: '#F93073',
+				},
+			},
 			animation: {
 				'ping-btn': 'ping-btn 1.5s 1s cubic-bezier(0, 0, 0.2, 1) infinite',
+				'btn-pulse': 'btn-pulse 2s infinite',
 			},
 			keyframes: {
 				'ping-btn': {
 					'75%, 100%': { transform: 'scale(1.2)', opacity: '0' },
+				},
+				'btn-pulse': {
+					'0%': { 'box-shadow': '0 0 0 0 rgba(249, 48, 115, 0.6)' },
+					'100%': { 'box-shadow': '0 0 4px 15px rgba(249, 48, 115, 0)' },
 				},
 			},
 		},
